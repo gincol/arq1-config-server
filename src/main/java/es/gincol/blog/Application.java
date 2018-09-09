1a0ce1f8871a81ec2d018d8d69be55c65b1ddafc
gines collado <gines.collado@gmail.com>
2018-09-09 12:23:51 +0200

    eureka en bootstrap

--- src/main/java/es/gincol/blog/Application.java
+++ src/main/java/es/gincol/blog/Application.java
@@ -7,8 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
 
@@ -16,7 +16,7 @@ import es.gincol.blog.utils.Constants;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
