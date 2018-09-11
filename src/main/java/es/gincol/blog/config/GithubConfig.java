330d07d33c784615a74b5f1b113c21fd3b77659c
gines collado <gines.collado@gmail.com>
2018-09-11 09:14:14 +0200

    cambio estructura

---
+++ src/main/java/es/gincol/blog/config/GithubConfig.java
@@ -0,0 +1,13 @@
+//package es.gincol.blog.config;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class GithubConfig {
+//	@Bean
+//	public GithubPropertyPathNotificationExtractor githubPropertyPathNotificationExtractor() {
+//		return new GithubPropertyPathNotificationExtractor();
+//	}
+//}
